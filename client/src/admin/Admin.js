491c59380ce0af788d63758f94455f1e494e77d4
juanangelaalma <juanlah77@gmail.com>
2021-10-12 07:01:34 +0700

    create link on sidebar admin

--- client/src/admin/Admin.js
+++ client/src/admin/Admin.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { Route, Switch } from "react-router";
 import { Link } from "react-router-dom";
 import Blogs from "./pages/Blogs";
+import Category from "./pages/Category";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 const Admin = () => {
   const [isOpenSidebar, setIsOpenSidebar] = useState(true);
@@ -16,6 +18,12 @@ const Admin = () => {
       </Route>
       <Route path="/admin/blog">
         <Blogs {...sidebar}  />
+      </Route>
+      <Route path="/admin/category">
+        <Category {...sidebar}  />
+      </Route>
+      <Route path="/admin/profile">
+        <Profile {...sidebar}  />
       </Route>
     </Switch>
   );
--- client/src/admin/components/SideBar.js
+++ client/src/admin/components/SideBar.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/sidebar.css";
 
 const SideBar = ({ isOpenSidebar }) => {
   return (
     <aside className={isOpenSidebar ? 'seen' : 'hidden'}>
       <div className="sidebar">
-        <a className="brand" href="#">
+        <Link className="brand" to='/'>
           ALMA<span>.</span>
-        </a>
+        </Link>
         <div className="side-links d-flex w-100 py-2">
-          <a href="" className="side-item py-2 px-5">
+          <Link to="/admin/dashboard" className="side-item py-2 px-5">
             <i class="fas fa-tachometer-alt mr-2"></i> Dashboard
-          </a>
-          <a href="" className="side-item py-2 px-5">
+          </Link>
+          <Link to="/admin/blog" className="side-item py-2 px-5">
             <i class="fas fa-pencil-alt mr-2"></i> Blog
-          </a>
-          <a href="" className="side-item py-2 px-5">
+          </Link>
+          <Link to="/admin/category" className="side-item py-2 px-5">
             <i class="fas fa-list mr-2"></i> Category
-          </a>
-          <a href="" className="side-item py-2 px-5">
+          </Link>
+          <Link to="/admin/profile" className="side-item py-2 px-5">
             <i class="fas fa-user-lock mr-2"></i> Profile
-          </a>
+          </Link>
         </div>
       </div>
     </aside>
