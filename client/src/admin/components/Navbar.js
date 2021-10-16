3c53b501c8d85c0871387af6b5e22f25cd96edd3
juanangelaalma <juanlah77@gmail.com>
2021-10-16 08:09:48 +0700

    change style on navbar menu admin

--- client/src/admin/components/Navbar.js
+++ client/src/admin/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = ({ children, isOpenSidebar, setIsOpenSidebar }) => {
   return (
     <nav className="navbar navbar-expand-lg p-0 navbar-dark position-fixed" style={{ width: isOpenSidebar ? '80%' : '100%' }}>
       <button
-        className={ isOpenSidebar ? "navbar-toggler navbar-toggler-right" : "navbar-toggler" }
+        className="navbar-toggler d-flex align-items-center custom-navbar-toggler"
         type="button"
         data-toggle="collapse"
         aria-expanded="false"
         aria-label="Toggle navigation"
         onClick={() => setIsOpenSidebar(!isOpenSidebar)}
       >
-        <span className="navbar-toggler-icon" />
+        <span className={ isOpenSidebar ? "fas fa-ellipsis-h vertical" : "fas fa-ellipsis-h" }/>
       </button>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ml-auto">
