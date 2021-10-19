7be4349dff43c89f98d3345f3954d13bb0189cab
juanangelaalma <juanlah77@gmail.com>
2021-10-19 23:00:56 +0700

    added auth handler

--- client/src/user/pages/login/Login.js
+++ client/src/user/pages/login/Login.js
@@ -1,28 +1,39 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
-import { login } from '../../../actions/auth'
+import { login } from "../../../actions/auth";
+import { notifyFail } from "../../../common/toast";
 import "./styles.css";
 
 const Login = () => {
-  const [email, setEmail] =  useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
-  const dispatch = useDispatch()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-  const user = useSelector(state => state.auth)
+  const user = useSelector((state) => state.auth);
+  const message = useSelector((state) => state.message)
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setLoading(true)
-    dispatch(login({ email, password })).then(() => setLoading(false)).catch(() => setLoading(false))
-  }
+    e.preventDefault();
+    setLoading(true);
+    dispatch(login({ email, password }))
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
+  };
 
-  if(user.isLoggedIn) {
-    return <Redirect to="/admin/dashboard" />
-  }
+  useEffect(() => {
+    if(message && !(user.isLoggedIn)) {
+      notifyFail(message.message)
+    }
+  }, [message])
 
+  if (user.isLoggedIn) {
+    return <Redirect to="/admin/dashboard" />;
+  }
   return (
     <div className="container login-page">
       <div className="row justify-content-center align-items-center">
@@ -32,16 +43,39 @@ const Login = () => {
           </p>
           <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} id="email" type="text" className="form-control" />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              type="text"
+              className="form-control"
+            />
             <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} id="password" type="password" className="form-control" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              type="password"
+              className="form-control"
+            />
             <button type="submit" className="btn px-4 text-white">
-              { loading ? 'loading...' : 'Login' }
+              {loading ? "loading..." : "Login"}
             </button>
           </form>
         </div>
         <Shapes />
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
